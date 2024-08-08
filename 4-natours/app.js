24673413d369782a0a167cf1c089596aa3ba947c
emilykamiti <emilykamiti@gmail.com>
2024-08-08 13:39:47 +0300

    API - delete requet

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -76,6 +76,21 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    ///chnage the status code to no content
+    status: 'success',
+    data: null,
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
