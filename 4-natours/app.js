ad8693849da3343d857555f8493df6f8107a7e0f
emilykamiti <emilykamiti@gmail.com>
2024-08-22 12:47:01 +0300

    servicing static variables files that cannot be access using the router eg. a file.css or file.html

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
-
+const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // Middlewares
+app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the  middleware 👋 ');
