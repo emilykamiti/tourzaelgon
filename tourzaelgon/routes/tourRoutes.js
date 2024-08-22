1e03a6445c72072df1236c86f6bfe2d45fd5c7c8
emilykamiti <emilykamiti@gmail.com>
2024-08-22 15:15:19 +0300

    updating older name

---
+++ tourzaelgon/routes/tourRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const tourController = require('../controllers/tourController.js');
+
+const router = express.Router();
+
+router.param('id', tourController.checkID);
+
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.checkBody, tourController.createTour);
+
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
+
+module.exports = router;
+
+//param middleware -middleware that only runs for certain parameters .
