cc7689529e8b79e2d75f1395f46b7af1c85ebb6b
emilykamiti <emilykamiti@gmail.com>
2024-08-26 13:53:23 +0300

    connecting the database with the express application

--- tourzaelgon/server.js
+++ tourzaelgon/server.js
@@ -1,7 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DBConnection successful');
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
