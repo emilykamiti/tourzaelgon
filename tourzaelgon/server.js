fe7dc643f0f181bfc9cdff07cddef2f7faef2875
emilykamiti <emilykamiti@gmail.com>
2024-08-28 16:53:31 +0300

    creating a Tour model

--- tourzaelgon/server.js
+++ tourzaelgon/server.js
@@ -9,16 +9,36 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 );
 
+mongoose.set('strictQuery', false); //please watch out for this line
+
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
   })
   .then(() => {
     console.log('DBConnection successful');
   });
+
+const tourSchema = new mongoose.Shcema({
+  //specifying shcema
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'], // validator
+    unique: true,
+  },
+
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+
+  price: {
+    type: Number,
+    required: [true, 'The tour must have a price'],
+  },
+});
+const Tour = mongoose.node('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
