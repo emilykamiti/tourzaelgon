c7a9e3da22305d9f378ae80f6bee8eeb3169338e
emilykamiti <emilykamiti@gmail.com>
2024-08-30 15:56:07 +0300

    PATCH: Updating data using mongoose

--- tourzaelgon/controllers/tourController.js
+++ tourzaelgon/controllers/tourController.js
@@ -56,13 +56,25 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '< Updated tour here ...>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, //return updateed document
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invlid input',
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
