eeb99f56eec1f5649c641c07afe1d9af2cbd6d40
emilykamiti <emilykamiti@gmail.com>
2024-09-05 16:24:51 +0300

    making the api better-filtering

--- tourzaelgon/controllers/tourController.js
+++ tourzaelgon/controllers/tourController.js
@@ -2,13 +2,25 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    console.log(req.query);
+    //BUILD A QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
-    const tours = await Tour.find({
-      duration: 5,
-      difficulty: 'easy',
-    });
+    console.log(req.query, queryObj);
 
+    const query = Tour.find(queryObj);
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    //EXECUTE QUERY
+    const tours = await Tour.find();
+
+    //SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
