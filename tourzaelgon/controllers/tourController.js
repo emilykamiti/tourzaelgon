fa27ce8b2c54c160be86a02a0d8e760a5c220fb9
emilykamiti <emilykamiti@gmail.com>
2024-09-06 09:54:07 +0300

    advanced filtering

--- tourzaelgon/controllers/tourController.js
+++ tourzaelgon/controllers/tourController.js
@@ -3,22 +3,20 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     //BUILD A QUERY
+    //(1) filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    console.log(req.query, queryObj);
+    //(2) advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
 
-    const query = Tour.find(queryObj);
-
-    // const tours = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
+    const query = Tour.find(JSON.parse(queryStr));
 
     //EXECUTE QUERY
-    const tours = await Tour.find();
+    const tours = await query;
 
     //SEND RESPONSE
     res.status(200).json({
