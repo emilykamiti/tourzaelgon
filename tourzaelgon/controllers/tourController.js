d7e6d44f61679088cfeac83757e9e565bfc784fa
emilykamiti <emilykamiti@gmail.com>
2024-09-12 16:51:16 +0300

    match multiple times

--- tourzaelgon/controllers/tourController.js
+++ tourzaelgon/controllers/tourController.js
@@ -113,13 +113,19 @@ exports.getTourStats = async (req, res) => {
       },
       {
         $group: {
-          _id: null,
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
           avgRating: { $avg: '$ratingsAverage' },
           avgPrice: { $avg: '$price' },
           minPrice: { $min: '$price' },
           maxPrice: { $max: '$price' },
         },
       },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      { $match: { _id: { $ne: 'EASY' } } },
     ]);
 
     res.status(200).json({
