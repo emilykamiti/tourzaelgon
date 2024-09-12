cb9241e957501fb9338f401f14e77e3cf739601f
emilykamiti <emilykamiti@gmail.com>
2024-09-12 16:52:38 +0300

    Aggregation pipeline and grouping

--- tourzaelgon/controllers/tourController.js
+++ tourzaelgon/controllers/tourController.js
@@ -125,7 +125,7 @@ exports.getTourStats = async (req, res) => {
       {
         $sort: { avgPrice: 1 },
       },
-      { $match: { _id: { $ne: 'EASY' } } },
+      // { $match: { _id: { $ne: 'EASY' } } }, //! you can match multiple times
     ]);
 
     res.status(200).json({
