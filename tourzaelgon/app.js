fd049c601172ac22d0297440673117c3363f63f9
emilykamiti <emilykamiti@gmail.com>
2024-09-19 18:06:48 +0300

    implementing a global error handling

--- tourzaelgon/app.js
+++ tourzaelgon/app.js
@@ -23,9 +23,23 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `can't find ${req.originalUrl} on this server`,
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `can't find ${req.originalUrl} on this server`,
+  // });
+  const err = new Error(`can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
