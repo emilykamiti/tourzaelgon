b5ab82d548a53fde137aa3d4970a358c221241c5
emilykamiti <emilykamiti@gmail.com>
2024-09-19 17:37:18 +0300

    handling unhandled routes

--- tourzaelgon/app.js
+++ tourzaelgon/app.js
@@ -14,11 +14,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the  middleware 👋 ');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -26,5 +21,12 @@ app.use((req, res, next) => {
 // (3) Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `can't find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
