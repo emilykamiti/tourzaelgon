c9bea6ac00fc517b8f62eb943619817b67e0acb7
emilykamiti <emilykamiti@gmail.com>
2024-10-08 16:22:32 +0300

    handleJWTExpiredError

--- tourzaelgon/controllers/errorController.js
+++ tourzaelgon/controllers/errorController.js
@@ -20,6 +20,13 @@ const handleValidationErrorDB = (err) => {
   const message = `Invalid input data. ${errors.join(', ')}`;
   return new AppError(message, 400);
 };
+
+const handleJWTError = () =>
+  new AppError('Invalid token please login again', 401);
+
+const handleJWTExpiredError = () =>
+  new AppError('Your token has expired, please try again', 401);
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -62,6 +69,9 @@ module.exports = (err, req, res, next) => {
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);
+
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
     sendErrorProd(error, res);
   }
