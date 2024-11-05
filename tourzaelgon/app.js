910fd29407318b9e05aa0df5b7b4ad88c883d883
emilykamiti <emilykamiti@gmail.com>
2024-11-05 20:31:48 +0300

    Preventing parameter pollution

--- tourzaelgon/app.js
+++ tourzaelgon/app.js
@@ -4,7 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
-
+const hpp = require('hpp');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -37,6 +37,20 @@ app.use(mongoSanitize());
 
 // Data sanitization against xss
 app.use(xss());
+
+//prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
 
 //serving static files
 app.use(express.static(`${__dirname}/public`));
