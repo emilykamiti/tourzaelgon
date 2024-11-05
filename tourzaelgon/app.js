0ab3e5e6db26b4fbfbe880e76bb5a0114ddc741c
emilykamiti <emilykamiti@gmail.com>
2024-11-05 17:00:22 +0300

    setting security Http headers

--- tourzaelgon/app.js
+++ tourzaelgon/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,9 +11,14 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // Glpobal  Middlewares
+// Set security http headers
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+//limit request from same api
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -20,9 +26,14 @@ const limiter = rateLimit({
 });
 
 app.use('/api', limiter);
-app.use(express.json());
+
+//Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//serving static files
 app.use(express.static(`${__dirname}/public`));
 
+//test middle ware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
