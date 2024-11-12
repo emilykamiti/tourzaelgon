9fffed1234789e1a847ed8a8937a132c4ff7a62f
emilykamiti <emilykamiti@gmail.com>
2024-11-12 08:43:22 +0300

    query middleware

--- tourzaelgon/models/tourModel.js
+++ tourzaelgon/models/tourModel.js
@@ -140,6 +140,19 @@ tourSchema.pre('save', function (next) {
 //   console.log(doc);
 //   next();
 // });
+
+//QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`query took ${Date.now() - this.start} milliseconds`);
+  console.log(docs);
+  next();
+});
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
