d45d7bd4646f409fa999948829c207e499536d63
emilykamiti <emilykamiti@gmail.com>
2024-11-13 10:54:53 +0300

    populating tour guides

--- tourzaelgon/models/tourModel.js
+++ tourzaelgon/models/tourModel.js
@@ -156,6 +156,15 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt',
+  });
+
+  next();
+});
+
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`query took ${Date.now() - this.start} milliseconds`);
   console.log(docs);
