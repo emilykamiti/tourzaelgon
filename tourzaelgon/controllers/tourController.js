75efcb2c8103ea9040592cde3d588489d7ba44a7
emilykamiti <emilykamiti@gmail.com>
2024-11-13 10:22:23 +0300

    populating tour guides: can create other queriries in the background, could be a problem

--- tourzaelgon/controllers/tourController.js
+++ tourzaelgon/controllers/tourController.js
@@ -30,7 +30,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id);
+  const tour = await Tour.findById(req.params.id).populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt',
+  });
 
   if (!tour) {
     return next(new AppError('No tour found with that ID', 404));
@@ -138,6 +141,7 @@ exports.getMonthlyplan = catchAsync(async (req, res) => {
         tours: { $push: `$name` },
       },
     },
+
     {
       $addFields: { month: '$_id' },
     },
