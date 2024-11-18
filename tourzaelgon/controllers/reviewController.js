82f5e5804d43cfdd8841e6f59cd51575664063cf
emilykamiti <emilykamiti@gmail.com>
2024-11-18 21:05:41 +0300

    building handler factory functions delete

--- tourzaelgon/controllers/reviewController.js
+++ tourzaelgon/controllers/reviewController.js
@@ -1,7 +1,7 @@
 const Review = require('../models/reviewModel');
-// const APIFeatures = require('../utils/apiFeatures');
+
 const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
+const factory = require('./handlerFactory');
 
 // exports.aliasTopReviews = (req, res, next) => {
 //   req.query.limit = '5';
@@ -38,15 +38,4 @@ exports.createReview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.deleteReview = catchAsync(async (req, res, next) => {
-  const review = await Review.findByIdAndDelete(req.params.id);
-
-  if (!review) {
-    return next(new AppError('No review found with that ID', 404));
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-});
+exports.deleteReview = factory.deleteOne(Review);
