acff272ca6b6590aa30ee9233fefab213ad49e0f
emilykamiti <emilykamiti@gmail.com>
2024-11-19 22:08:12 +0300

    adding a me endpoint: getting current user details

--- tourzaelgon/controllers/userController.js
+++ tourzaelgon/controllers/userController.js
@@ -11,6 +11,11 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.updateMe = catchAsync(async (req, res, next) => {
   //1) create error if user posts password data
   if (req.body.password || req.body.passwordConfirm) {
