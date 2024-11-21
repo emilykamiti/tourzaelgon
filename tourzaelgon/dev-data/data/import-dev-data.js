2517c879b8cac9c2ce498acecf01acc893631098
emilykamiti <emilykamiti@gmail.com>
2024-11-21 16:20:24 +0300

    importing review and user data

--- tourzaelgon/dev-data/data/import-dev-data.js
+++ tourzaelgon/dev-data/data/import-dev-data.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../../models/tourModel');
-
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 // Load environment variables from config.env
 dotenv.config({ path: '../../config.env' });
 
@@ -25,13 +26,18 @@ mongoose
 
 // READ JSON FILE
 const toursPath = `${__dirname}/tours.json`;
-console.log('Tours JSON Path:', toursPath);
-const tours = JSON.parse(fs.readFileSync(toursPath, 'utf-8'));
+const usersPath = `${__dirname}/users.json`;
+const reviewsPath = `${__dirname}/reviews.json`;
 
-// IMPORT DATA INTO DB
+const tours = JSON.parse(fs.readFileSync(toursPath, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(usersPath, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(reviewsPath, 'utf-8'));
+
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfully loaded');
   } catch (err) {
     console.log('Error loading data:', err.message);
@@ -43,6 +49,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany({});
+    await User.deleteMany({});
+    await Review.deleteMany({});
     console.log('Data successfully deleted');
   } catch (err) {
     console.log('Error deleting data:', err.message);
