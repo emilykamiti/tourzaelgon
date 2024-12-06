76a8fa55315df2de5c8950a464a84f60c2de0a86
emilykamiti <emilykamiti@gmail.com>
2024-12-06 19:44:04 +0300

    first steps with pug

--- tourzaelgon/app.js
+++ tourzaelgon/app.js
@@ -70,7 +70,10 @@ app.use((req, res, next) => {
 // (3) Routes
 
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Nasinza',
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
